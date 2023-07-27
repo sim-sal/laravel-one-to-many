3328c608accac06f188b0a8b5448dfa6ac22da48
sim-sal <simone.saladino93@gmail.com>
2023-07-27 03:49:20 +0200

    completed connection with table "bridge"

--- database/migrations/9999_07_25_174656_add_foreign_keys.php
+++ database/migrations/9999_07_25_174656_add_foreign_keys.php
@@ -30,8 +30,17 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
+
             $table->dropForeign('projects_type_id_foreign');
             $table->dropColumn('type_id');
+        });
+        Schema::table('project_technologie', function (Blueprint $table) {
+
+            $table->dropForeign('project_technologie_project_id_foreign');
+            $table->dropForeign('project_technologie_technologie_id_foreign');
+
+            $table->dropColumn('project_id');
+            $table->dropColumn('technologie_id');
         });
     }
 };

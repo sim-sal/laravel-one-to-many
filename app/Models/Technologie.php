b5e72b7ea9907fd67097cefd3190482a0915c98b
sim-sal <simone.saladino93@gmail.com>
2023-07-27 04:07:09 +0200

    created new MODEL and defined the relationships

---
+++ app/Models/Technologie.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Project;
+
+class Technologie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "description"
+    ];
+
+    public function projects()
+    {
+        return $this->belongsToMany(Project::class);
+    }
+}
